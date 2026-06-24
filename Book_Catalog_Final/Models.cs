8c62c45d2d3ab9ed8550c4c17dc3c56a7a138851
Alexsei-web <aleksei.ilin.07@mail.ru>
2026-06-24 19:58:03 +0300

    Добавлен класс ListBoxItem для хранения данных книги

---
+++ Book_Catalog_Final/Models.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Catalog_Final
+{
+    internal class Models
+    {
+    }
+}
